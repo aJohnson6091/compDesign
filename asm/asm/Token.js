1705e4b6c39b68156aebf921c9bc39b7c0b54c90
Austin Johnson <austin.johnson.6091@gmail.com>
2020-05-06 22:39:12 -0400

    Init commit

---
+++ asm/asm/Token.js
@@ -0,0 +1,11 @@
+class Token {
+    constructor(sym, line, lexeme) {
+        this.sym = sym;
+        this.line = line;
+        this.lexeme = lexeme;
+    }
+    toString() {
+        return `${this.sym} ${this.line} ${this.lexeme}`;
+    }
+}
+//# sourceMappingURL=Token.js.map
---
+++ asm/asm/TreeNode.js
@@ -0,0 +1,8 @@
+class TreeNode {
+    constructor(sym, token) {
+        this.sym = sym;
+        this.token = token;
+        this.children = [];
+    }
+}
+//# sourceMappingURL=TreeNode.js.map
